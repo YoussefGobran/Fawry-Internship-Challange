26f472256dc9f3ee697bb303fd1d6995966d6abc
Youssef <youssef.saad.gobran@gmail.com>
2025-07-05 23:17:16 +0300

    all classes done

--- FawryChallange/src/main/java/org/example/Customer.java
+++ FawryChallange/src/main/java/org/example/Customer.java
@@ -6,9 +6,33 @@ public class Customer {
     private double balance;
     private String address;
 
+    public Customer(String name, double balance, String address) {
+        this.name = name;
+        this.balance = balance;
+        this.address = address;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public double getBalance() {
         return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
---
+++ FawryChallange/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.Date;
+
+public class Main {
+    public  static Date  today = new Date(2025,7,5);
+
+    public static void main(String[] args) {
+
+    }
+}
